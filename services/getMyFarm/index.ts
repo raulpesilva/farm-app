ea3258d9a420c990e123fdd734609a438d56374d
raulpesilva <adraulpesilva@gmail.com>
2025-09-20 22:52:09 -0300

    feat: remove console log from getMyFarm service for cleaner output

--- services/getMyFarm/index.ts
+++ services/getMyFarm/index.ts
@@ -3,6 +3,5 @@ import { coreApi } from '@/api';
 
 export const getMyFarm = async () => {
   const response = await coreApi.get<Farm>('/farm');
-  console.log({ response });
   return response.data;
 };
