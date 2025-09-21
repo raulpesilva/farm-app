f4416255f376272e6a3396afaef1a3a7bcea13fd
raulpesilva <adraulpesilva@gmail.com>
2025-09-21 19:35:51 -0300

    refactor: update deleteProduct service to use coreApi for product deletion and remove unnecessary logic

--- services/deleteProduct/index.ts
+++ services/deleteProduct/index.ts
@@ -1,8 +1,11 @@
-import { sleep } from '@/utils';
-import { getProducts } from '../getProducts';
+import { ProductItem } from '@/@types/product';
+import { coreApi } from '@/api';
+import { getFarm } from '@/states';
 
 export const deleteProduct = async (id: number) => {
-  await sleep(150);
-  const prev = await getProducts();
-  return prev.filter((product) => product.id !== id);
+  const farm = getFarm();
+  if (!farm) throw new Error('Farm not found');
+
+  const response = await coreApi.delete<ProductItem>(`/product/${farm.id}/${id}`);
+  return response.data;
 };
