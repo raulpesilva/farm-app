a4887ad8dd382e5c02ee4f1419f46ea7c4dfe917
raulpesilva <adraulpesilva@gmail.com>
2025-09-21 03:29:35 -0300

    feat: update mock data for goals and sales to use new structure and types

--- mocks/goals/index.ts
+++ mocks/goals/index.ts
@@ -1,30 +1,44 @@
 import { GoalItem } from '@/@types/goal';
 
 export const GOALS_MOCK: GoalItem[] = [
-  {
-    id: 1,
-    product_id: 1,
-    title: 'Meta 1',
-    measure: 'quantity',
-    type: 'buy',
-    value: 0,
-    target: 10,
-    completed: null,
-    notified: null,
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-  {
-    id: 2,
-    product_id: 1,
-    title: 'Meta 2',
-    measure: 'quantity',
-    type: 'sell',
-    value: 10,
-    target: 10,
-    completed: new Date(),
-    notified: null,
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-];
+    {
+        "farm_id": 1,
+        "product_id": 5,
+        "name": "Ttt",
+        "type": "sale",
+        "measure": "quantity",
+        "target": 10000,
+        "value": 1000,
+        "id": 3,
+        "created_at": "2025-09-21T06:25:57.270Z",
+        "updated_at": "2025-09-21T06:26:55.194Z"
+    },
+    {
+        "farm_id": 1,
+        "product_id": 4,
+        "name": "bla",
+        "type": "storage",
+        "measure": "quantity",
+        "target": 1000,
+        "value": 1000,
+        "id": 2,
+        "created_at": "2025-09-21T05:03:17.666Z",
+        "updated_at": "2025-09-21T05:05:55.061Z",
+        "completed": "2025-09-21T05:05:55.061Z",
+        "notified": "2025-09-21T05:05:55.061Z"
+    },
+    {
+        "farm_id": 1,
+        "product_id": 1,
+        "name": "meta 1",
+        "type": "storage",
+        "measure": "quantity",
+        "target": 1000,
+        "value": 1000,
+        "id": 1,
+        "created_at": "2025-09-21T04:01:27.910Z",
+        "updated_at": "2025-09-21T05:05:38.224Z",
+        "completed": "2025-09-21T05:05:38.224Z",
+        "notified": "2025-09-21T05:05:38.224Z"
+    }
+]
