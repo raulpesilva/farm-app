46caefff599a5ddf7905d6d065b1402d1f541240
raulpesilva <adraulpesilva@gmail.com>
2025-09-22 20:53:19 -0300

    fix: add regex utility functions for string manipulation

--- utils/index.ts
+++ utils/index.ts
@@ -2,6 +2,7 @@ export * from './formatCurrency';
 export * from './groupByDate';
 export * from './groupByStock';
 export * from './masks';
+export * from './regex';
 export * from './removeEmptySearchParam';
 export * from './sleep';
 
